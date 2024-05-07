723254f05817b87d023767a420e961151f3c1f45
alessiorubicini <alessiorubicini05@gmail.com>
2024-05-07 14:03:13 +0200

    Fixes

--- Sources/SwiftSessions/Chan.swift
+++ Sources/SwiftSessions/Chan.swift
@@ -1,15 +1,13 @@
 import Foundation
 import AsyncAlgorithms
 
-class Chan<A, B> {
+final class Chan<A, B> {
     let channel: AsyncChannel<AnyObject>
     
     init(channel: AsyncChannel<AnyObject>) {
         self.channel = channel
     }
-}
-
-extension Chan {
+    
     static func create() -> (Chan<A, B>, Chan<B, A>) {
         let channel: AsyncChannel<AnyObject> = AsyncChannel()
         let c1 = Chan<A, B>(channel: channel)
@@ -17,16 +15,22 @@ extension Chan {
         return (c1, c2)
     }
     
-    static func send(_ payload: A, on channel: consuming Chan<A, B>) async {
-        await channel.channel.send(payload as AnyObject)
-        channel.channel.finish()
+    static func send<A, B, C>(_ payload: A, on chan: consuming Chan<(A, Chan<B, C>), Empty>) async -> Chan<C, B> {
+        // Invio il payload sul canale
+        await chan.channel.send(payload as AnyObject)
+        chan.channel.finish()
+        // Restituisco la continuazione, cioè il canale originale ma con tipo aggiornato
+        return chan as! Chan<C, B>
+    }
+
+    static func recv<A, B, C>(from chan: consuming Chan<Empty, (A, Chan<B, C>)>) async -> (A, Chan<B, C>) {
+        // Ricevo il messaggio dal canale
+        let msg = await chan.channel.first(where: { _ in true })!
+        // Restituisco il messaggio e la continuazione, cioè il canale originale con tipo aggiornato
+        return (msg as! A, chan as! Chan<B, C>)
     }
     
-    static func recv(from channel: consuming Chan<B, A>) async -> AnyObject {
-        return await channel.channel.first(where: { _ in true })!
-    }
-    
-    static func close(channel: consuming Chan<A, B>) async {
+    static func close(channel: consuming Chan<Empty, Empty>) async {
         return
     }
 }
--- Tests/SwiftSessionsTests/SwiftSessionsTests.swift
+++ Tests/SwiftSessionsTests/SwiftSessionsTests.swift
@@ -4,20 +4,23 @@ import XCTest
 @testable import SwiftSessions
 
 final class SwiftSessionsTests: XCTestCase {
-    func test1() async throws {
+    func test1() async {
         typealias Session = Chan<(Int, Chan<Bool, Empty>), Empty>
+        var (c1, c2) = Session.create()
         
-        let (c1, c2) = Session.create()
+        // Invio l'intero
+        let c3 = await Session.send(42, on: c1)
         
-        Task {
-            let continuation = Chan<Bool, Empty>(channel: c1.channel)
-            await Session.send((42, continuation), on: c1)
-        }
+        // Ricevo l'intero e la continuazione
+        let (num, send_c) = await Session.recv(from: c2)
 
-        Task {
-            let res = await Session.recv(from: c2)
-            print("RESULT: [\(res)]")
-        }
+        // Invio il booleano
+        let isEven = (num % 2 == 0)
+        let c4 = await Session.send(isEven, on: send_c)
+        
+        // Ricevo il booleano
+        let b = await Session.recv(from: c4)
+        print("Result: \(b)")
     }
 }
 
