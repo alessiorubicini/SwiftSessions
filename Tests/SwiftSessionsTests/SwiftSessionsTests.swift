148ab853ffc594c3521588c7f9db887ed375428c
alessiorubicini <alessiorubicini05@gmail.com>
2024-05-11 22:31:04 +0200

    added guard for re-declaration

--- Tests/SwiftSessionsTests/SwiftSessionsTests.swift
+++ Tests/SwiftSessionsTests/SwiftSessionsTests.swift
@@ -4,7 +4,7 @@ import XCTest
 @testable import SwiftSessions
 
 final class SwiftSessionsTests: XCTestCase {
-    func testIsEven() async {
+    func testIsEvenWithoutGuard() async {
         typealias Session = Chan<(Int, Chan<(Bool, Chan<Empty, Empty>), Empty>), Empty>
         
         let c = await Session.create({ c in
@@ -18,6 +18,23 @@ final class SwiftSessionsTests: XCTestCase {
         let (isEven, c2) = await Session.recv(from: c1)
         
         Session.close(c2)
+        
+        assert(isEven == true)
+    }
+    func testIsEvenWithGuard() async {
+        typealias Session = Chan<(Int, Chan<(Bool, Chan<Empty, Empty>), Empty>), Empty>
+        
+        let c = await Session.create({ c in
+            let (num, c) = await Session.recv(from: c)
+            let end = await Session.send(num % 2 == 0, on: c)
+            Session.close(end)
+        })
+        
+        guard case let c = await Session.send(42, on: c) else {}
+        
+        guard case let (isEven, c) = await Session.recv(from: c) else {}
+        
+        Session.close(c)
         
         assert(isEven == true)
     }
