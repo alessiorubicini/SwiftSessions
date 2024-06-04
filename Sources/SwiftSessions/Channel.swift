1485820ddd381b278d44e1db487678fb467bf612
alessiorubicini <alessiorubicini05@gmail.com>
2024-06-04 10:21:57 +0200

    improved linearity checks

--- Sources/SwiftSessions/Channel.swift
+++ Sources/SwiftSessions/Channel.swift
@@ -31,15 +31,6 @@ public final class Channel<A, B>: @unchecked Sendable {
     /// Once set to `true`, this property cannot be changed back to `false`.
     private(set) var isConsumed: Bool = false
     
-    /// A human-readable description of the channel, including the message types.
-    ///
-    /// This property returns a string representation of the channel, specifying the types of messages it can send (`A`) and receive (`B`).
-    ///
-    /// - Returns: A string representation of the channel in the format "Channel<A, B>".
-    public var description: String {
-        "Channel<\(A.self), \(B.self)>"
-    }
-    
     /// Initializes a new channel with the given asynchronous channel.
     /// - Parameter channel: The underlying asynchronous channel for communication.
     init(with channel: AsyncChannel<Sendable>) {
@@ -63,7 +54,7 @@ public final class Channel<A, B>: @unchecked Sendable {
     
     /// Marks the channel as consumed, ensuring linearity guarantees.
     ///
-    /// This method is called internally before sending or receiving on the channel.
+    /// This method is called internally before a primitive is executed on the channel.
     /// It throws an error if the channel has already been consumed,
     /// enforcing the linear usage pattern of session types.
     ///
@@ -79,6 +70,15 @@ public final class Channel<A, B>: @unchecked Sendable {
                 fatalError(error.localizedDescription)
             }
         }
+    }
+    
+    /// A human-readable description of the channel, including the message types.
+    ///
+    /// This property returns a string representation of the channel, specifying the types of messages it can send (`A`) and receive (`B`).
+    ///
+    /// - Returns: A string representation of the channel in the format "Channel<A, B>".
+    public var description: String {
+        "Channel<\(A.self), \(B.self)>"
     }
     
 }
